8b397d5e11eb808102b57a2b71c3b3919415a6f1
david <david.budnitsky,2001@gmail.com>
2023-12-29 13:55:53 +0200

    Test2

--- pythonProject2/main.py
+++ pythonProject2/main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+import numpy as np
+from matplotlib import pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+x = np.arange(0, 2*np.pi, 0.01)
+y = np.sin(3*x)
+y1 = np.sin(2*x)
 
+plt.plot(x, y)
+plt.plot(x, y1)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+plt.show()
